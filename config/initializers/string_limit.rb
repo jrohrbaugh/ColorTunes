e6592d2c1429bdcbfd147a41405d6f57fcd32bf8
Josh Rohrbaugh <jwr4db@virginia.edu>
2012-04-11 20:09:03 -0400

    added initializer

---
+++ config/initializers/string_limit.rb
@@ -0,0 +1,26 @@
+module ActiveRecord::ConnectionAdapters
+
+  class PostgreSQLAdapter < AbstractAdapter
+
+    # Monkey patched to remove varchar limit of 255 that Rails set, since
+    # it makes no performance difference in Postgres.
+    def native_database_types #:nodoc:
+      {
+        :primary_key => "serial primary key",
+        :string      => { :name => "character varying" },
+        :text        => { :name => "text" },
+        :integer     => { :name => "integer" },
+        :float       => { :name => "float" },
+        :decimal     => { :name => "decimal" },
+        :datetime    => { :name => "timestamp" },
+        :timestamp   => { :name => "timestamp" },
+        :time        => { :name => "time" },
+        :date        => { :name => "date" },
+        :binary      => { :name => "bytea" },
+        :boolean     => { :name => "boolean" }
+      }
+    end
+
+  end
+
+end
